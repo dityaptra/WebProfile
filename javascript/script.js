a4241b66101be1009aa86c5dd7fae094df7dace4
Sir Naomi <gdevelop351@gmail.com>
2024-11-21 21:09:03 +0800

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.teks-bergerak', {
+    strings: ['Frontend Developer','Backend Developer','Full Stack Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+})
