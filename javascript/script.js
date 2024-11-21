61b8bdba9b3ffcc7c2acaab2fb4dea092b9b415e
Gede Raditya Putra <134995449+dityaptra@users.noreply.github.com>
2024-11-21 21:23:37 +0800

    ubah kembali

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 const typed = new Typed('.teks-bergerak', {
-    strings: ['Backend Developer','Full Stack Developer'],
+    strings: ['Frontend Developer','Backend Developer','Full Stack Developer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 100,
